1895fa5a1272c73b2ef538e3448b81a86cc8419d
unknown <kanou_tom@yahoo.gr>
2021-12-10 19:29:15 +0200

    Delete S4_TF-IDF_v2 script

--- Scripts/S4_TF-IDF_v2.py
+++
@@ -1,7 +0,0 @@
-import sys
-from collections import Counter
-from Old_Scripts.tokenizer import tokenize
-from S1_corpus_bigrams_occurences import count_occur
-from S2_corpus_bigrams_unique_occurences import count_unique_occur
-
-sys.path.insert(0,'C:\CodeRepository\Formatting-Error-Correction\Scripts')
