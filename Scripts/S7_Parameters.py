71205f22bfce4d8fc7788a1a940bf0410a0669e2
ThomasKanoutas <kanou_tom@yahoo.gr>
2023-02-12 14:49:22 +0200

    [MODIFIED]: Modified S7_Parameters file in order to add a new parameter called tabSpace which represents the configured tabspace that is being used in the project. Currently, a tabspace is consisted of 4 spaces.

--- Scripts/S7_Parameters.py
+++ Scripts/S7_Parameters.py
@@ -5,21 +5,32 @@ tokensAvailable = ["<start>","<end>","<eos>","<tab>","<spacetab>","<dot>","<comm
                     "<plus>","<equal>","<open_bracket>","<close_bracket>","<open_curly>","<close_curly>","<less_than>",
                     "<greater_than>","<quest>","<back_slash>","<keyword>","<literal>","<lit>","<word>","<space>",
                     "<com>","<slash>","<string>","<char>","<number>","<unk>"]
+
 # A list of all the replacements that can be a possible fix
 replacements = ["","","\n","\t","    ",".",",",";",":","!","@","#","$", "%","^","&","*","(",")","-","+","=","[","]","{",
                "}","<",">","?","\\","for ","true ","int "," abc "," ","/* comment */","/"," abc123 ","a",
                "123","<unk>"]
-# Number of suggested fixes
-numOfSuggFixes = 3 
-# Number of tokens that will be checked for the formating error
-numOfCheckedTok = 3
-# Represents the number of tokens that its code snippet will be consisted of
-tokensPerSnippet = 20
-# Step that will be used to produce N-grams. It is used during tokens scoring in order to reduce total calculation time
-ngramStep = 3
+
 # Dictionary that maps tokens to tokens ID
 tokensMapping = dict((c, i) for i, c in enumerate(tokensAvailable))
+
+# Number of suggested fixes
+numOfSuggFixes = 3
+
+# Number of tokens that will be checked for the formating error
+numOfCheckedTok = 3
+
+# Represents the number of tokens that its code snippet will be consisted of
+tokensPerSnippet = 20
+
+# Step that will be used to produce N-grams. It is used during tokens scoring in order to reduce total calculation time
+ngramStep = 3
+
 # Main project path
 path = r'/Users/Shared/c/CodeRepository/Formatting-Error-Correction/'
+
 # Path that fixed source code files are saved
 fixDir = "/Users/Shared/c/CodeRepository/Formatting-Error-Correction/Fixes/"
+
+# Configured tabspace, used during fixing
+tabSpace = '    '
