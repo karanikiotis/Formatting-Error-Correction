d2417b599b1c0bf4e0253c0a9e67488ef83c3285
ThomasKanoutas <kanou_tom@yahoo.gr>
2023-04-08 19:36:46 +0300

    [MODIFIED]: Modified S7_Parameters.py in order to add a new parameter regarding if possible error positions will be filtered according to error probability threshold or by choosing the top K tokens with highest error probabilities. Moreover, modified comments.

--- Scripts/S7_Parameters.py
+++ Scripts/S7_Parameters.py
@@ -6,37 +6,41 @@ tokensAvailable = ["<start>","<end>","<eos>","<tab>","<spacetab>","<dot>","<comm
                     "<greater_than>","<quest>","<back_slash>","<keyword>","<literal>","<lit>","<word>","<space>",
                     "<com>","<slash>","<string>","<char>","<number>","<unk>"]
 
-# A list of all the replacements that can be a possible fix
+# A list of all the replacements that can be a possible fix.
 replacements = ["","","\n","\t","    ",".",",",";",":","!","@","#","$", "%","^","&","*","(",")","-","+","=","[","]","{",
                "}","<",">","?","\\","for ","true ","int "," abc "," ","/* comment */","/"," abc123 ","a",
                "123","<unk>"]
 
-# Dictionary that maps tokens to tokens ID
+# Dictionary that maps tokens to tokens ID.
 tokensMapping = dict((c, i) for i, c in enumerate(tokensAvailable))
 
-# Number of suggested fixes
+# Number of suggested fixes.
 numOfSuggFixes = 3
 
-# Number of tokens that will be taken into consideration as detected positions for possible formating error
-numOfCheckedTok = 10
+# Number of tokens that will be taken into consideration as detected positions for possible formating error.
+numOfCheckedTok = 5
 
-# Represents the number of tokens that its code snippet will be consisted of
+# Represents the number of tokens that its code snippet will be consisted of.
 tokensPerSnippet = 20
 
-# Step that will be used to produce N-grams. It is used during tokens scoring in order to reduce total calculation time
+# Step that will be used to produce N-grams. It is used during tokens scoring in order to reduce total calculation time.
 ngramStep = 3
 
-# Main project path
+# Main project path.
 path = r'/Users/Shared/c/CodeRepository/Formatting-Error-Correction/'
 
-# Path that fixed source code files are saved
+# Path that fixed source code files are saved.
 fixDir = "/Users/Shared/c/CodeRepository/Formatting-Error-Correction/Fixes/"
 
-# Configured tabspace, used during fixing
+# Configured tabspace, used during fixing.
 tabSpace = '    '
 
 # Probability Threshold. Only tokens with error probabilities above this threshold are taken into consideration for fixing
-probThresh = 0.9
+probThresh = 0.75
 
-# Return to the user only the fixed file with the best score
+# Return to the user only the fixed file with the best score.
 returnBestScoreFile = False
+
+# This parameter defines whether we are going to filter possible error positions according to the probability threshold
+# or by choosing the numofCheckedTok first tokens.
+probThreshActive = False
